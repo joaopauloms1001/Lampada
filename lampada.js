724a77dd5bb96efcaafac8bed64b028f536525d4
João Paulo Marins Santos <joaopaulomarinssantos@gmail.com>
2023-02-14 21:45:12 -0300

    14/02/23
    Foi adicionado mais uma maneira na escrita do código para inserir a imagem da luz apagada e luz acesa

--- lampada.js
+++ lampada.js
@@ -1,6 +1,8 @@
 function apagar(){
-    img.setAttribute('src', 'imagens/apagada.jpg')
+    //img.setAttribute('src', 'imagens/apagada.jpg')
+    img.src = 'imagens/apagada.jpg'
 }
 function acender(){
-    img.setAttribute('src', 'imagens/acesa.jpg')
+    //img.setAttribute('src', 'imagens/acesa.jpg')
+    img.src = 'imagens/acesa.jpg'
 }
